73b02bd4ea17b8f4abedbd64acdcb6d97e9f9375
godsarmy <godsarmycy@gmail.com>
2015-03-18 23:06:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     description='Python API to get Chinese stock price',
     author='Walt Chen',
     author_email='godsarmycy@gmail.com',
-    url='https://www.python.org/sigs/distutils-sig/',
+    url='https://www.python.org/godsarmy/cstock/',
     packages=['cstock'],
+    platforms = 'any',
 )
