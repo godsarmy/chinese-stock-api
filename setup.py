f3f3513060de36a311b5fe655c34fcae27d749cb
godsarmy <godsarmycy@gmail.com>
2015-03-18 23:33:14 +0800

    Update README and setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name='chinesestockapi',
-    version='1.0',
+    version='1.1',
     description='Python API to get Chinese stock price',
     author='Walt Chen',
     author_email='godsarmycy@gmail.com',
-    url='https://www.python.org/godsarmy/cstock/',
+    url='https://pypi.python.org/pypi/chinesestockapi',
     packages=['cstock'],
     platforms = 'any',
 )
