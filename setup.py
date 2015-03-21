43a7bdb217bf5461038cf949d26f5477291a2ee5
godsarmy <godsarmycy@gmail.com>
2015-03-22 00:08:35 +0800

    Fix long description in setup.py

--- setup.py
+++ setup.py
@@ -14,12 +14,15 @@ Supported Engines:
 Usage:
 
  from cstock.request import Requests
+
  from cstock.hexun_engine import HexunEngine
 
  engine = HexunEngine()
+
  requester = Requester(engine)
 
  stock = requester.request('000626')
+
  print stock.as_dict()
 
 Download the latest release: https://github.com/godsarmy/chinese-stock-api/zipball/master.
